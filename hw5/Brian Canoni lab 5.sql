1f6a784dbcc6ce2f76d5e57b932d500e82c6a6e1
Brian Canoni <bcanoni@gmail.com>
2014-02-21 04:03:15 -0500

    Lab 5 Brian Canoni
    More sql practice

---
+++ hw5/Brian Canoni lab 5.sql
@@ -0,0 +1,41 @@
+﻿--Brian Canoni Lab 5
+
+
+-- 1
+select city from Agents
+join Orders
+on Orders.aid = Agents.aid
+where Orders.cid = 'c002';
+
+-- 2 
+select pid from Orders
+join Agents
+on Orders.aid = Agents.aid
+where Orders.cid = 'c006';
+
+--3
+select name from Customers
+where cid not in (
+		select cid from Orders
+		 );
+
+--4
+select Customers.name from Customers
+left outer join Orders
+on Customers.cid=Orders.cid
+where Orders.cid is null;
+
+--5 
+select Customers.name, Agents.name from Customers,Agents
+where Customers.city = Agents.city;
+
+-6
+select Customers.name, Agents.name, Customers.city from Customers,Agents
+where Customers.city = Agents.city;
+
+-7
+select distinct Customers.name,Customers.city from Customers
+join Products
+on Products.city = Customers.city
+where Products.city = 'Duluth';
+
